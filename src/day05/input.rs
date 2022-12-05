18ae12b03c5da079520ecfdf75411788f6da2541
Jueun Grace Yun <graceyunn@gmail.com>
2022-12-05 01:47:52 -0500

    Format input

--- src/day05/input.rs
+++ src/day05/input.rs
@@ -12,7 +12,43 @@ pub fn read_example() -> Input {
 }
 
 pub fn read_internal(input: &str) -> Input {
-    unimplemented!()
+    let stacks_and_instructions: Vec<&str> = input.split("\n\n").collect();
+    let mut stacks = stacks_and_instructions[0];
+    let mut instructions = stacks_and_instructions[1];
+    let stacks = format_stacks(stacks);
+    let instructions = format_instructions(instructions);
+    (stacks, instructions).into()
+}
+
+pub fn format_stacks(rows: &str) -> Vec<Vec<char>> {
+    let mut rows: Vec<&str> = rows.split("\n").collect();
+    rows.pop();
+    let row_length = rows[0].len();
+    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); (row_length + 1) / 4];
+    for row in rows {
+        let row: Vec<char> = row.chars().collect();
+        let mut index = 0;
+        for n in (1..row_length + 1).step_by(4) {
+            if !row[n].is_whitespace() {
+                stacks[index].push(row[n]);
+            }
+            index += 1;
+        }
+    }
+   
+    stacks.iter_mut().for_each(|s| s.reverse());
+    (stacks)
+}
+
+pub fn format_instructions(rows: &str) -> Vec<[usize; 3]> {
+    let mut instructions: Vec<[usize; 3]> = Vec::new();
+    let rows: Vec<&str> = rows.split("\n").collect();
+    for row in rows {
+        let row: Vec<&str> = row.split(" ").collect();
+        let row: [usize; 3] = [row[1].parse().unwrap(), row[3].parse().unwrap(), row[5].parse().unwrap()];
+        instructions.push(row);
+    }
+    (instructions)
 }
 
 #[cfg(test)]
